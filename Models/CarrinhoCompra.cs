80d5214f38f50b55e4e1c69e07111d0ad4a0e781
Simeao Almeida <simer27@yahoo.com>
2024-06-06 14:02:57 -0300

    Método para adicionar um item ao carrinho ou se já existir, apenas aumento a quantidade em 1

--- Models/CarrinhoCompra.cs
+++ Models/CarrinhoCompra.cs
@@ -1,6 +1,60 @@
-﻿namespace Lanches_Online.Models
+﻿using Lanches_Online.Context;
+
+namespace Lanches_Online.Models
 {
     public class CarrinhoCompra
     {
+        private readonly AppDbContext _context;
+
+        public CarrinhoCompra(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public string? CarrinhoCompraId { get; set; }
+        public List<CarrinhoCompraItem>? carrinhoCompraItems { get; set; }
+
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            ISession session = 
+                services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+
+        public void AdicionarAoCarrinho( Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                s => s.Lanche.LancheId == lanche.LancheId &&
+                s.CarrinhoCompraId == CarrinhoCompraId);
+
+            if (carrinhoCompraItem == null)
+            {
+                carrinhoCompraItem = new CarrinhoCompraItem
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1
+                };
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+            _context.SaveChanges();
+
+        }
+
     }
 }
