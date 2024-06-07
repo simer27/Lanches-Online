ab16f4aeae35b4d024126f5873aa6ff325ad70cd
Simeao Almeida <simer27@yahoo.com>
2024-06-07 15:18:20 -0300

    criação da HomeViewModel

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Lanches_Online.Models;
+
+namespace Lanches_Online.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set;}
+    }
+}
