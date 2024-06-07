51a053f4bcfd5b44f4931a559f62899e4d6a97b9
Simeao Almeida <simer27@yahoo.com>
2024-06-07 15:10:42 -0300

    Criação da ViewModel de CarrinhoCompra

---
+++ ViewModels/CarrinhoCompraViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Lanches_Online.Models;
+
+namespace Lanches_Online.ViewModels
+{
+    public class CarrinhoCompraViewModel
+    {
+        public CarrinhoCompra? CarrinhoCompra { get; set; }
+        public decimal CarrinhoCompraTotal { get; set; }
+    }
+}
