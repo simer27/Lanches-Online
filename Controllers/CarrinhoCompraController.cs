9ac27e2f833f83130d628efede27e8fab1b0dabd
Simeao Almeida <simer27@yahoo.com>
2024-06-07 15:10:02 -0300

    Criação da Controller CarrinhoCompras com os métodos index, adicionar ao carrinho e remover do carrinho

---
+++ Controllers/CarrinhoCompraController.cs
@@ -0,0 +1,60 @@
+﻿using Lanches_Online.Models;
+using Lanches_Online.Repositories.Interfaces;
+using Lanches_Online.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+
+namespace Lanches_Online.Controllers
+{
+    public class CarrinhoCompraController : Controller
+    {
+
+        private readonly ILancheRepository _lancheRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraController(ILancheRepository lancheRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _lancheRepository = lancheRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Index()
+        {
+
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.carrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal(),
+            };
+            return View(carrinhoCompraVM);
+        }
+
+        public RedirectToActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
