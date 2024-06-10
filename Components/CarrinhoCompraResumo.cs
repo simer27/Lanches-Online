c9782a9882770e4280405d4174f105376187219d
Simeao Almeida <simer27@yahoo.com>
2024-06-10 12:53:42 -0300

    criação da Pasta Components na pasta Raiz,
    criação da Pasta Components e arquivo CarrinhoCompraResumo na pasta Views/Shared
    configuração da navbar na pasta Shared/Layout

---
+++ Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,31 @@
+﻿using Lanches_Online.Models;
+using Lanches_Online.ViewModels;
+using Lanches_Online.Views.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches_Online.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.carrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal(),
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
