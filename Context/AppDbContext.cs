bae7cebc532b8a9c4de9baac0e18c405e54e2eab
Simeao Almeida <simer27@yahoo.com>
2024-06-12 19:36:44 -0300

    Criação das models Pedidos e PedidoDetalhes
    Criação Dbset para Pedidos e PedidoDetalhes
    Migration Executada

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -12,5 +12,8 @@ namespace Lanches_Online.Context
         public DbSet<Categoria>? Categorias { get; set; }
         public DbSet<Lanche>? Lanches { get; set; }
         public DbSet<CarrinhoCompraItem>? CarrinhoCompraItens { get; set; }
+
+        public DbSet<Pedido>? Pedidos { get; set;}
+        public DbSet<PedidoDetalhe>? pedidoDetalhes { get; set; }
     }
 }
---
+++ Models/Pedido.cs
@@ -0,0 +1,73 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lanches_Online.Models
+{
+    public class Pedido
+    {
+        public int PedidoId { get; set; }
+
+        [Required(ErrorMessage ="Informe o Nome")]
+        [StringLength(50)]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe o Sobrenome")]
+        [StringLength(50)]
+        public string? Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Informe o Endereço")]
+        [StringLength(100)]
+        [Display(Name = "Endereço")]
+        public string? Endereco1 { get; set; }
+
+        [Required(ErrorMessage = "Informe o Endereço")]
+        [StringLength(100)]
+        [Display(Name = "Complemento")]
+        public string? Endereco2 { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu CEP")]
+        [StringLength(10, MinimumLength = 8)]
+        [Display(Name = "CEP")]
+        public string? Cep { get; set; }
+
+        [StringLength(10)]
+        public string? Estado { get; set; }
+
+        [StringLength(50)]
+        public string? Cidade { get; set; }
+
+        [Required(ErrorMessage = "Informe o seu Telefone")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        public string? Telefone { get; set; }
+
+        [Required(ErrorMessage = "Informe seu email")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$)",
+        ErrorMessage = "O email não possui um formato correto")]
+        public string? Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Column(TypeName = "decimal(12,2)")]
+        [Display(Name = "Total do Pedido")]
+        public decimal PedidoTotal { get; set; }
+
+        [ScaffoldColumn(false)]
+        [Display(Name = "Itens no Pedido")]
+        public int TotalItensPedido { get; set; }
+
+        [Display(Name = "Data do Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm", ApplyFormatInEditMode = true)]
+        public DateTime PedidoEnviado { get; set; }
+
+        [Display(Name = "Data Envio Pedido")]
+        [DataType(DataType.Text)]
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy hh:mm", ApplyFormatInEditMode = true)]
+        public DateTime? PedidoEntregueEm { get; set; }
+
+
+        public List<PedidoDetalhe>? PedidoItens { get; set; }
+    }
+}
